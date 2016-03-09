10142d651286f2e6270a6eda5476c2ffa12fa991
nolivia <nolivia@nolivia-macbookpro.roam.corp.google.com>
2016-03-09 11:49:07 -0800

    init tf formula

---
+++ tensorflow.rb
@@ -0,0 +1,35 @@
+class Tensorflow < Formula
+  desc ""
+  homepage "https://tensorflow.org"
+  url "https://github.com/tensorflow/tensorflow/archive/v0.7.1.tar.gz"
+  version "0.7.1"
+  sha256 "ef34121432f7a522cf9f99a56cdd86e370cc5fa3ee31255ca7cb17f36b8dfc0d"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test tensorflow`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
