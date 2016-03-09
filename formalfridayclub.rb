f0e9af9b9f09e94c8e20ccfc31821e6a10fbd261
nolivia <nolivia@nolivia-macbookpro.roam.corp.google.com>
2016-03-09 11:19:59 -0800

    testing taps?

---
+++ formalfridayclub.rb
@@ -0,0 +1,11 @@
+class Formalfridayclub < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/itsmeolivia/formalfridayclub/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "1b1f0acb1e20032b93c2f2d31ed2460eda3495358f521aa3786ba5fdf9b10557"
+
+  def install
+    bin.install "formalfriday"
+  end
+end
